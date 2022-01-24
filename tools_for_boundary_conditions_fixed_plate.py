2f09d6c7a310ecc49714e687123001dbcf43efae
Tsvetelina Ivanova <ivanova@ealin34.private.vki.eu>
2022-01-24 18:55:39 +0100

    Boundary conditions in the case of fixed plate and perturbations introduced in the middle of the domain.

---
+++ tools_for_boundary_conditions_fixed_plate.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 17:33:33 2021
+
+@author: ivanova
+
+
+Definition of a function for imposing linear extrapolation
+as boundary conditions for the Liquid film solver.
+"""
+
+#################################################
+def linear_extrap_lrt(h, qx, qz):
+    '''
+    Boundary conditions: linear extrapolation.
+    This function enforces linear extrapolation
+    on the left, right, and top boundaries (lrt)
+    (sides and outlet respectively)
+    of the moving substrate.
+    '''
+    # LEFT AND RIGHT BOUNDARIES (SLICES):
+
+    # Linear extrapolation along z
+    qz[:,0]  = 2*qz[:,1] - qz[:,2]
+    qx[:,0]  = 2*qx[:,1] - qx[:,2]
+    h[:,0]   = 2*h[:,1]  - h[:,2]
+
+    qz[:,-1] = 2*qz[:,-2] - qz[:,-3]
+    qx[:,-1] = 2*qx[:,-2] - qx[:,-3]
+    h[:,-1]  = 2*h[:,-2]  - h[:,-3]
+
+    # TOP BOUNDARY (OUTLET):
+
+    # Linear extrapolation along x
+    qz[0,:]  = 2*qz[1,:] - qz[2,:]
+    qx[0,:]  = 2*qx[1,:] - qx[2,:]
+    h[0,:]   = 2*h[1,:] - h[2,:]
+
+	# BOTTOM BOUNDART (INLET):
+
+	# Linear extrapolation along x
+    qz[-1,:]  = 2*qz[-2,:] - qz[-3,:]
+    qx[-1,:]  = 2*qx[-2,:] - qx[-3,:]
+    h[-1,:]   = 2*h[-2,:] - h[-3,:]
